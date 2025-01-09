108965624fc7bc79596adaf4fab3516e0f01f445
Saragesh <balaji.saragesh@gmail.com>
2025-01-09 21:18:21 +0530

    updated signin

--- signin.py
+++ signin.py
@@ -3,8 +3,9 @@ from firebase_admin import credentials, auth, firestore
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
-firebase_credentials = {
+class FirebaseAuth:
+    def __init__(self):
+        self.firebase_credentials = {
             "type": os.getenv("FIREBASE_TYPE"),
             "project_id": os.getenv("FIREBASE_PROJECT_ID"),
             "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
@@ -17,31 +18,30 @@ firebase_credentials = {
             "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
             "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
         }
+        cred = credentials.Certificate(self.firebase_credentials)
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
 
-cred = credentials.Certificate(firebase_credentials)
-firebase_admin.initialize_app(cred)
+    def get_user_id(self, email, password):
+        try:
+            user = auth.get_user_by_email(email)
+            return user.uid
+        except Exception as e:
+            print(f"Error getting user: {e}")
+            return None
 
-db = firestore.client()
+    def get_user_info_firestore(self, user_id):
+        try:
+            user_data = self.db.collection('user_data').document(user_id).get()
+            return user_data.to_dict()
+        except Exception as e:
+            print(f"Error getting user data: {e}")
+            return None
 
-def get_user_id (email, password):
-    try:
-        user = auth.get_user_by_email(email)
-        return user.uid
-    except Exception as e:
-        print(f"Error getting user: {e}")
-        return None
-
-def get_user_info_firestore(user_id):
-    try:
-        user_data = db.collection('user_data').document(user_id).get()
-        return user_data.to_dict()
-    except Exception as e:
-        print(f"Error getting user data: {e}")
-        return None
-    
-email = "babloo23@gmail.com"
-password = "babloo123"
-user_id = get_user_id(email, password)
-if user_id:
-    print(f"User Id: {get_user_id(email, password)}")
-   
+if __name__ == "__main__":
+    email = "babloo23@gmail.com"
+    password = "babloo123"
+    firebase_auth = FirebaseAuth()
+    user_id = firebase_auth.get_user_id(email, password)
+    if user_id:
+        print(f"User Id: {user_id}")
